ee8d0a669fbdd3b7cb3e46d969838d4785b533d8
Thanh Nguyen <btnguyen2k@gmail.com>
2024-01-13 16:10:04 +1100

    Test: refactor test data directory

---
+++ module_test/dummy_test.go
@@ -0,0 +1,19 @@
+package gocosmos_test
+
+import (
+	"testing"
+)
+
+func TestDummy(t *testing.T) {
+	t.SkipNow()
+	//f, err := os.OpenFile("testdata/volcano.json", os.O_CREATE|os.O_RDWR, 0644)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//defer f.Close()
+	//n, err := f.WriteString(_testDataVolcano)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//t.Logf("wrote %d/%d bytes", n, len(_testDataVolcano))
+}
